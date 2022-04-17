15d081f2ccda29473cf0e17d6fc02cbf2159a489
d34dl0ck <serg.ipatov.si@gmail.com>
2022-04-17 14:35:27 +0300

    added test for container

---
+++ internal/container/container_test.go
@@ -0,0 +1,27 @@
+package container_test
+
+import (
+	"testing"
+
+	"github.com/d34dl0ck/coupler/internal/container"
+	"github.com/d34dl0ck/coupler/internal/container/testdata"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestResolveWithStrategy(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	expected := "some_expected_obj"
+
+	c := container.NewContainer()
+	k := container.NewRawResolvingKey("some_key")
+	strategyMock := testdata.NewMockResolvingStrategy(ctrl)
+	strategyMock.EXPECT().Resolve(gomock.Len(1)).Return(expected, nil)
+	c.Register(k, strategyMock)
+
+	actual, err := c.Resolve(k)
+	require.NoError(t, err, "err was not expected")
+	require.Equal(t, expected, actual)
+}
