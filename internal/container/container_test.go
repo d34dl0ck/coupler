a56e562fe563e43aafe264952f23b41f2cb61c33
d34dl0ck <serg.ipatov.si@gmail.com>
2022-04-17 23:31:36 +0300

    finished tests for container

--- internal/container/container_test.go
+++ internal/container/container_test.go
@@ -25,3 +25,32 @@ func TestResolveWithStrategy(t *testing.T) {
 	require.NoError(t, err, "err was not expected")
 	require.Equal(t, expected, actual)
 }
+
+func TestConflictSolvedWithOverwrite(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	expected := "some_expected_obj"
+
+	c := container.NewContainer()
+	k := container.NewRawResolvingKey("some_key")
+	strategyMock := testdata.NewMockResolvingStrategy(ctrl)
+	strategyMock.EXPECT().Resolve(gomock.Len(1)).Return(expected, nil)
+	c.Register(k, strategyMock)
+	c.Register(k, strategyMock)
+
+	actual, err := c.Resolve(k)
+	require.NoError(t, err, "err was not expected")
+	require.Equal(t, expected, actual)
+}
+
+func TestErrMissingKey(t *testing.T) {
+	t.Parallel()
+
+	c := container.NewContainer()
+	k := container.NewRawResolvingKey("some_key")
+
+	actual, err := c.Resolve(k)
+	require.Nil(t, actual, "actual should be nil")
+	require.ErrorIs(t, err, container.ErrKeyNotRegistered)
+}
